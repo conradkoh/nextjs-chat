60bfd9f964d95464855c2d7782cd486c5b818089
conradkoh <conradkoh@gmail.com>
2023-06-22 15:46:29 +0800

    attempt: disable caching to fix auth issues

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,8 @@
-export { auth as middleware } from './auth'
+import { auth } from './auth'
+import { NextRequest, NextResponse } from 'next/server'
+export const middleware = (req: NextRequest) => {
+  const res = NextResponse.next()
+  res.headers.set('Cache-Control', 'no-store, max-age=0')
+  //TODO: this is a hack to ensure that the next-auth can use the edge middleware interfaces. testing only
+  return auth(req as any, res as any)
+}
