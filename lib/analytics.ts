3de2fdfe334c86f1f47a7e9f3341581d860b7a39
conradkoh <conradkoh@gmail.com>
2023-06-24 21:54:53 +0800

    disable analytics

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -12,51 +12,47 @@ export const initAnalytics = ({
 
   return {
     track: async (eventName: string, data?: any) => {
-      try {
-        if (!endpoint && process.env.NODE_ENV === 'development') {
-          console.log(
-            `[Vercel Web Analytics] Track "${eventName}"` +
-              (data ? ` with data ${JSON.stringify(data || {})}` : '')
-          )
-          return
-        }
-
-        const headers: { [key: string]: string } = {}
-        Object.entries(request.headers).map(([key, value]) => {
-          headers[key] = value
-        })
-
-        const body = {
-          o: headers.referer,
-          ts: new Date().getTime(),
-          r: '',
-          en: eventName,
-          ed: data
-        }
-
-        const promise = fetch(
-          `https://${process.env.VERCEL_URL}/_vercel/insights/event`,
-          {
-            headers: {
-              'content-type': 'application/json',
-              'user-agent': headers['user-agent'] as string,
-              'x-forwarded-for': headers['x-forwarded-for'] as string,
-              'x-va-server': '1'
-            },
-            body: JSON.stringify(body),
-            method: 'POST'
-          }
-        )
-
-        if (event) {
-          event.waitUntil(promise)
-        }
-        {
-          await promise
-        }
-      } catch (err) {
-        console.error(err)
-      }
+      // try {
+      //   if (!endpoint && process.env.NODE_ENV === 'development') {
+      //     console.log(
+      //       `[Vercel Web Analytics] Track "${eventName}"` +
+      //         (data ? ` with data ${JSON.stringify(data || {})}` : '')
+      //     )
+      //     return
+      //   }
+      //   const headers: { [key: string]: string } = {}
+      //   Object.entries(request.headers).map(([key, value]) => {
+      //     headers[key] = value
+      //   })
+      //   const body = {
+      //     o: headers.referer,
+      //     ts: new Date().getTime(),
+      //     r: '',
+      //     en: eventName,
+      //     ed: data
+      //   }
+      //   const promise = fetch(
+      //     `https://${process.env.VERCEL_URL}/_vercel/insights/event`,
+      //     {
+      //       headers: {
+      //         'content-type': 'application/json',
+      //         'user-agent': headers['user-agent'] as string,
+      //         'x-forwarded-for': headers['x-forwarded-for'] as string,
+      //         'x-va-server': '1'
+      //       },
+      //       body: JSON.stringify(body),
+      //       method: 'POST'
+      //     }
+      //   )
+      //   if (event) {
+      //     event.waitUntil(promise)
+      //   }
+      //   {
+      //     await promise
+      //   }
+      // } catch (err) {
+      //   console.error(err)
+      // }
     }
   }
 }
